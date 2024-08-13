365c3939310d5e5dc5d6704e8ec7f7ad63154e36
Magnum Opus <arifraihan062003@gmail.com>
2024-08-14 01:11:36 +0800

    Added Win Chance, Multiplier Tab and Invert Button

--- app.js
+++ app.js
@@ -1,21 +1,70 @@
+
+let inversemode = false;
+let posColor = "green";
+let negColor = "red";
+let firstClick = true;
+
+let changePoint = 50;
+
 let movingSlider = document.querySelector(".moving-slider");
-document.querySelector("#rangeInput").oninput = function() {
-    var valueDisplay = (this.value-this.min)/(this.max-this.min)*100 + 1;
-    this.style.background = 'linear-gradient(to right, green 0%, green ' + valueDisplay + '%, red ' + valueDisplay + '%, red 100%)'
-    
+let winChanceInput = document.querySelector("#winChanceInput");
+let multiplierInput = document.querySelector("#multiplierInput");
+let inverseButton = document.querySelector("#inverse-button");
+let rangeInput = document.querySelector("#rangeInput");
+let edge = 0.05;
+
+inverseButton.addEventListener("click", function() {
+  const tempColor = posColor;
+  posColor = negColor;
+  negColor = tempColor;
+  inversemode = !inversemode;  
+  var valueDisplay = (rangeInput.value-rangeInput.min)/(rangeInput.max-rangeInput.min)*100 ;
+  rangeInput.style.background = `linear-gradient(to right, ${posColor} 0%, ${posColor} ${valueDisplay}%, ${negColor} ${valueDisplay}%, ${negColor} 100%)`;
+
+})
+rangeInput.oninput = function() {
+    var valueDisplay = (this.value-this.min)/(this.max-this.min)*100 ;
+    this.style.background = `linear-gradient(to right, ${posColor} 0%, ${posColor} ${valueDisplay}%, ${negColor} ${valueDisplay}%, ${negColor} 100%)`;
+
+    console.log(this.value);
+    changePoint = this.value;
+    generateWinChance(this.value);
+    generateMultiplier(this.value);
 };
+
+
+function generateWinChance(value){
+  winChanceInput.value = value;
+}
+
+function generateMultiplier(value){
+  multiplierInput.value = (1 / (value / 100) - edge).toFixed(2);
+}
 
 
 document.querySelector("#play-button").onclick = function() {
   const min = 0; 
-  const max = 95.5;
+  const max = 96;
 
   const randomNumber = parseFloat((Math.random() * 100).toFixed(2));
-  const sliderPosition = parseFloat((randomNumber / 100 * 95.5).toFixed(2));
+  const sliderPosition = parseFloat((randomNumber / 100 * max).toFixed(2));
+  
   movingSlider.style.left = `calc( ${sliderPosition}% - 8px)`;
-  movingSlider.textContent = randomNumber;
+  movingSlider.textContent = randomNumber.toFixed(2);
+  if(firstClick){
+    movingSlider.style.opacity = "1";
+    firstClick = false;
+  }
+  if(randomNumber > changePoint) {
+    movingSlider.style.color = `${negColor}`;
+    if(inversemode)console.log("Win");
+  }
+  else {
+    movingSlider.style.color = `${posColor}`;
+    if(!inversemode)console.log("Win");
+  }
   console.log(`calc( ${sliderPosition}%+ 10px - 18px)`);
-
+    
 
 
 }
